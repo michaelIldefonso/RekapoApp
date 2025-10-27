65b4baf3e405cefc2381d7e6614eb22f35affbcc
Michael Ildefonso <michaelildefonso8@gmail.com>
2025-10-27 21:11:51 +0800

    new start////

--- app/(tabs)/account.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function AccountScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Account Screen</Text>
-    </View>
-  );
-}
--- app/(tabs)/notes.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function NotesScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notes Screen</Text>
-    </View>
-  );
-}
